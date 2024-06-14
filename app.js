fa472dd220d3a5d0a527051ead23d0adc19a8ab2
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-06-14 11:25:20 -0400

    worked on the navbar from previous lab

---
+++ app.js
@@ -0,0 +1,57 @@
+// Menu data structure 
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+  // Select and cache the <main> element in a variable named mainEl.
+  
+  const mainEl = document.querySelector("main");
+  
+  // Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+  //Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+  const backColor = 'var(--main-bg)';
+  mainEl.style.backgroundColor = backColor;
+  
+  // Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+  mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+  
+  // Add a class of flex-ctr to mainEl.
+  // Hint: Use the Element.classList API
+  mainEl.classList = 'flex-ctr';
+  
+  // Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+  const topMenuEl = document.querySelector("#top-menu");
+  console.log(topMenuEl);
+
+  // Set the height of the topMenuEl element to be 100%.
+  topMenuEl.style.height = '100%';
+  
+  // Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+  let topMenuColor = 'var(--top-menu-bg)';
+  topMenuEl.style.backgroundColor = topMenuColor;
+  
+  
+  // Add a class of flex-around to topMenuEl.
+  topMenuEl.classList = 'flex-around';
+  
+  for( let i = 0; i < menuLinks.length; i++){
+      const menuAEl = document.createElement('a');
+      menuAEl.setAttribute('href',menuLinks[i]);
+      menuAEl.textContent = menuLinks[i].text;
+      topMenuEl.appendChild(menuAEl);
+  }
+
+  const subMenuEl = document.getElementById("sub-menu");
+  subMenuEl.style.height = '100%';
+  const subMenuColor = ' var(--sub-menu-bg)';
+  subMenuEl.style.backgroundColor = subMenuColor;
+  subMenuEl.classList = 'flex-around';
+  
+  
+  
+  
+  console.log(mainEl);
+  
+  console.log(subMenuEl);
